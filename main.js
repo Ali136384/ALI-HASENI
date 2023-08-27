918a40af17088cf768e1c6278026a00f8b3de7b8
Ali136384 <116725636+Ali136384@users.noreply.github.com>
2023-08-27 18:13:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ burger.onclick = () => {
   links.style.minWidth = "200%";
   links.style.marginBottom = "50px";
 };
+
+let up = document.getElementById("up");
+console.log(window.scrollY);
+setInterval(() => {
+  if (window.scrollY > 50) {
+    console.log(1);
+    up.style.right = "15px";
+    up.style.color = "#19cbfa";
+  } else {
+    up.style.right = "-100px";
+  }
+});
+
+up.addEventListener("click", () => {
+  window.scrollTo(0,0);
+});
