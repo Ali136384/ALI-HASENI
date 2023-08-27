ddf3115a67dc3f9e7588892a60b3fa6dce56198d
Ali136384 <116725636+Ali136384@users.noreply.github.com>
2023-08-27 23:32:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ let up = document.getElementById("up");
 console.log(window.scrollY);
 setInterval(() => {
   if (window.scrollY > 50) {
-    console.log(1);
     up.style.right = "15px";
     up.style.color = "#19cbfa";
   } else {
@@ -28,5 +27,38 @@ setInterval(() => {
 });
 
 up.addEventListener("click", () => {
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
 });
+
+let images = document.querySelectorAll(".imgs-container .box");
+
+let contires = document.querySelectorAll(".contries");
+
+contires.forEach((ele) => {
+  ele.addEventListener("click", (ele) => {
+    contires.forEach((e) => {
+      e.classList.remove("active");
+    });
+
+    ele.target.classList.add("active");
+  });
+});
+
+for (let i = 0; i < contires.length; i++) {
+  contires[i].onclick = () => {
+    let attr = contires[i].getAttribute("number");
+    for (let k = 0; k < images.length; k++) {
+      if (images[k].classList.contains(attr)) {
+        images[k].classList.remove("fal");
+
+        images[k].classList.add("tru");
+      } else if (contires[i].innerHTML === "All") {
+        images[k].classList.remove("fal");
+        images[k].classList.add("tru");
+      } else {
+        images[k].classList.add("fal");
+        images[k].classList.remove("tru");
+      }
+    }
+  };
+}
